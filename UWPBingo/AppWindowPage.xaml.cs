41d1709ee8741444e351ae8ec46c3153ec183019
PepijnHogervorst <pepijnh443@gmail.com>
2019-10-18 15:38:36 +0200

    #0 Changed project, startover

---
+++ UWPBingo/AppWindowPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPBingo
+{
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppWindowPage : Page
+    {
+        public AppWindowPage()
+        {
+            this.InitializeComponent();
+            this.SizeChanged += AppWindowPage_SizeChanged;
+        }
+
+        private void AppWindowPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawGrid();
+        }
+
+        private void BtnNextNumber_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnRestart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NumberGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            DrawHorizontalLine(lineH1, 1);
+            DrawHorizontalLine(lineH2, 2);
+            DrawHorizontalLine(lineH3, 3);
+            DrawHorizontalLine(lineH4, 4);
+            DrawHorizontalLine(lineH5, 5);
+            DrawHorizontalLine(lineH6, 6);
+            DrawHorizontalLine(lineH7, 7);
+            DrawHorizontalLine(lineH8, 8);
+            DrawHorizontalLine(lineH9, 9);
+
+            DrawVerticalLine(lineV1, 1);
+            DrawVerticalLine(lineV2, 2);
+            DrawVerticalLine(lineV3, 3);
+            DrawVerticalLine(lineV4, 4);
+            DrawVerticalLine(lineV5, 5);
+            DrawVerticalLine(lineV6, 6);
+            DrawVerticalLine(lineV7, 7);
+            DrawVerticalLine(lineV8, 8);
+            DrawVerticalLine(lineV9, 9);
+        }
+
+        private void DrawHorizontalLine(Line line, int position)
+        {
+            line.X1 = 0;
+            line.X2 = this.NumberGrid.ActualWidth;
+            line.Y1 = this.NumberGrid.ActualHeight / 10 * position;
+            line.Y2 = line.Y1;
+        }
+
+        private void DrawVerticalLine(Line line, int position)
+        {
+            line.X1 = this.NumberGrid.ActualWidth / 10 * position;
+            line.X2 = line.X1;
+            line.Y1 = 0;
+            line.Y2 = this.NumberGrid.ActualHeight;
+        }
+
+        
+    }
+}
---
+++ UWPBingo/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPBingo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Track open app windows in a Dictionary.
+        public static Dictionary<UIContext, AppWindow> AppWindows { get; set; }
+            = new Dictionary<UIContext, AppWindow>();
+
+        DispatcherTimer dispatcherTimer;
+        Ellipse bingoBall;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //Init timer to start second screen
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            dispatcherTimer.Start();
+
+            //Screen bounds 
+            var bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            var scaleFactor = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            var size = new Size(bounds.Width * scaleFactor, bounds.Height * scaleFactor);
+        }
+
+        private void DispatcherTimer_Tick(object sender, object e)
+        {
+            dispatcherTimer.Stop();
+            dispatcherTimer.Tick -= DispatcherTimer_Tick;
+
+            CreateSecondScreen();
+        }
+
+        private async void CreateSecondScreen()
+        {
+            // Create a new window.
+            AppWindow appWindow = await AppWindow.TryCreateAsync();
+
+            // Create a Frame and navigate to the Page you want to show in the new window.
+            Frame appWindowContentFrame = new Frame();
+            appWindowContentFrame.Navigate(typeof(AppWindowPage));
+
+            // Attach the XAML content to the window.
+            ElementCompositionPreview.SetAppWindowContent(appWindow, appWindowContentFrame);
+
+            // Add the new page to the Dictionary using the UIContext as the Key.
+            AppWindows.Add(appWindowContentFrame.UIContext, appWindow);
+            appWindow.Title = "App Window " + AppWindows.Count.ToString();
+
+            // When the window is closed, be sure to release
+            // XAML resources and the reference to the window.
+            appWindow.Closed += delegate
+            {
+                MainPage.AppWindows.Remove(appWindowContentFrame.UIContext);
+                appWindowContentFrame.Content = null;
+                appWindow = null;
+            };
+
+            // Show the window.
+            await appWindow.TryShowAsync();
+        }
+    }
+
+    public static class Globals
+    {
+
+    }
+}
